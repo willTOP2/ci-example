43f3f5d4d2ac54af5ac61615c86dd701c90b688b
Willian <willian.rodrygues146@gmail.com>
2021-07-11 18:33:56 -0300

    Adicionando Testes

--- test/utils.test.js
+++ test/utils.test.js
@@ -14,6 +14,12 @@ describe('Utils', () => {
 
         });
 
+        test('maior ou igual ao fim', () => {
+            expect(gerarNumeroAleatorio(45, 23))
+                .toBe(-1);
+
+        });
+
 
     });
 
@@ -23,11 +29,20 @@ describe('Utils', () => {
                 .toBe('comprimento invalido');
         });
 
-        // test('comprimento fornecido diferente do comprimento real', () => {
-        //         expect(acharCaracter(8, 'abcde', 'c'))
-        //            .toBe('comprimento fornecido diferente do comprimento real');
-        //    });
+        test('comprimento fornecido diferente do comprimento real', () => {
+            expect(acharCaracter(8, 'abcde', 'c'))
+                .toBe('comprimento fornecido diferente do comprimento real');
+        });
 
+        test('caracter não esta presente', () => {
+            expect(acharCaracter(5, 'abcde', 'r'))
+                .toBe('caracter nao encontrado');
+        });
+
+        test('posição do caracter', () => {
+            expect(acharCaracter(3, 'abc', 'a'))
+                .toBe(0);
+        });
 
 
 
