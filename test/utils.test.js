a4319b88e9a15777eade0d8df1eb6e9470d3ae8d
Willian <willian.rodrygues146@gmail.com>
2021-07-11 17:44:22 -0300

    commit

---
+++ test/utils.test.js
@@ -0,0 +1,17 @@
+const { gerarNumeroAleatorio, acharCaracter } = require('../lib/utils');
+
+describe('Utils', () => {
+    describe('gerarNumeroAleatorio', () => {
+        test('fim nao pode ser negativo', () => {
+            expect(gerarNumeroAleatorio(20, -5))
+                .toBe(-1);
+        });
+    });
+
+    describe('acharCaracter', () => {
+        test('comprimento invalido', () => {
+            expect(acharCaracter(-4, 'abcde', 'c'))
+                .toBe('comprimento invalido');
+        });
+    });    
+});
